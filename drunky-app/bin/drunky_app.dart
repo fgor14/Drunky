59414830a2f31aaf7914adc45af6831b3505d45e
LucaStudio22 <luca.bottoni.studio@gmail.com>
2023-03-23 15:07:24 +0100

    struttura drunky_app

--- drunky-app/bin/drunky_app.dart
+++
@@ -1,5 +0,0 @@
-import 'package:drunky_app/drunky_app.dart' as drunky_app;
-
-void main(List<String> arguments) {
-  print('Hello world: ${drunky_app.calculate()}!');
-}
---
+++ drunky_app/android/app/src/main/kotlin/com/example/drunky_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.drunky_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
